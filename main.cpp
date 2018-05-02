0578d7d9a4e9f55df0946cae57715e9e6dde681b
Andrey Shaj <kolobrod2002@yandex.ru>
2018-05-02 19:52:33 +0300

    В работе

--- main.cpp
+++ main.cpp
@@ -84,47 +84,47 @@ int main (int argc, char* argv[])
 
 
 
-		// matrix<int, 0> m;
-		// for(int i=0; i<10; i++)
-		// {
-		// 	m[i][i] = 9 - i;
-		// 	m[i][9-i] = 9 - i;
-		// }
+		my::matrix<int, 0> m;
+		for(int i=0; i<10; i++)
+		{
+			m[i][i] = 9 - i;
+			m[i][9-i] = 9 - i;
+		}
 
-		// for(int i=1; i<9; i++)
-		// {
-		// 	for(int j=1; j<9; j++)
-		// 	{
-		// 		std::cout << m[i][j] << " ";
-		// 	}
-		// 	std::cout << std::endl;
-		// }
+		for(int i=1; i<9; i++)
+		{
+			for(int j=1; j<9; j++)
+			{
+				std::cout << m[i][j] << " ";
+			}
+			std::cout << std::endl;
+		}
 
-		my::matrix<int, -10> m;
 
-		std::cout << "size = " << m.size() << std::endl;
-		// std::cout << m[1] << " - " << m[100000] << std::endl;
-		m[1] = 10;
-		std::cout << "size = " << m.size() << std::endl;
-
-		int a = m[1];
-		int b = m[2];
-		m[3] = 5;
-		m[4] = 500;
-		std::cout << "a = " << a << "; b = " << b << std::endl;
-		std::cout << "size = " << m.size() << std::endl;
-		std::cout << "m[1] " << m[1] << std::endl;
-		m[3] = -10;
-		std::cout << "size = " << m.size() << std::endl;
-		// std::cout << m[1] << " - " << m[100000] << std::endl;
-
-		// m[1] = -10;
-		// m[100000] = 100;
-		// std::cout << "size = " << m.size() << std::endl;
-		// std::cout << m[1] << " - " << m[100000] << std::endl;
-
+		for(auto it: m)
+		{
+			int x, y, z;
+			std::tie(x, y, v) = *it;
+			std::cout << x << y << v << std::endl;
+		}
 
 		BOOST_LOG_TRIVIAL(info) << "End matrix test";
+		// my::matrix<int, -10> m;
+
+		// std::cout << "size = " << m.size() << std::endl;
+		// // std::cout << m[1] << " - " << m[100000] << std::endl;
+		// m[1] = 10;
+		// std::cout << "size = " << m.size() << std::endl;
+
+		// int a = m[1];
+		// int b = m[2];
+		// m[3] = 5;
+		// m[4] = 500;
+		// std::cout << "a = " << a << "; b = " << b << std::endl;
+		// std::cout << "size = " << m.size() << std::endl;
+		// std::cout << "m[1] " << m[1] << std::endl;
+		// m[3] = -10;
+		// std::cout << "size = " << m.size() << std::endl;
 	}
 	catch(const std::exception &e)
 	{
--- matrix.h
+++ matrix.h
@@ -36,35 +36,44 @@ private:
 
 		using item = typename tabletype::item;
 		using value_type = typename tabletype::value_type;
+		using cellidx_t = typename tabletype::cell_idx_type;
 
 		tabletype *table;
 		std::size_t row_idx;
+
+		cellidx_t cell_idx {0, 0};
 
 		item_adapter(){};
 
 	public:
 
 		item_adapter(tabletype *tbl, std::size_t r)
-			:table(tbl), row_idx(r) {}
+			:table(tbl), row_idx(r) 
+		{
+			cell_idx.first = r;
+		}
 
 		item& operator=(const value_type &val)
 		{
 			BOOST_LOG_TRIVIAL(info) << __PRETTY_FUNCTION__;
 				
-			table->set(row_idx, val);
+			table->set(cell_idx, val);
 			return *this;
 		}
 
-		// U& operator[](int idx)
-		// {
-		// 	return m[0];
-		// }
+		item& operator[](int idx)
+		{
+			BOOST_LOG_TRIVIAL(info) << __PRETTY_FUNCTION__;
+
+			cell_idx.second = idx;
+			return *this;
+		}
 
 		operator const value_type&() const
 		{
 			BOOST_LOG_TRIVIAL(info) << __PRETTY_FUNCTION__;
 				
-			return table->get(row_idx);
+			return table->get(cell_idx);
 		}
 
 		// void set_idx(int idx)
@@ -73,18 +82,86 @@ private:
 		// }
 	};
 
+public: 
 
+	template<class tabletype>
+	class Iterator;
 
-	std::map<int, T> m;
+	using iterator = Iterator<T>;
+
+	using item = item_adapter<matrix<T, DEF_VAL, N>>;
+	using value_type = T;
+	using cell_idx_type = typename std::pair<std::size_t, std::size_t>;
+	using container_type = typename std::map<cell_idx_type, T>;
+	using iterator_type = typename container_type::iterator;
+	using const_iterator_type = typename container_type::const_iterator;
+
+	template<class tabletype>
+	class Iterator
+	{
+	private:
+
+		using itertype = typename tabletype::const_iterator_type;
+
+		tabletype *table;
+		itertype iter;
+
+		Iterator(){}
+
+	public:
+
+		using value_type = T;
+		using pointer = T*;
+		using const_pointer = const T*;
+		using reference = T&;
+		using const_reference = const T&;
+
+		Iterator(tabletype *tbl, itertype itr) : table(tbl), iter(itr)
+		{
+
+		}
+
+		// Проверка на равенство
+		bool operator==( const Iterator& other ) const 
+		{
+			if( this == &other ) 
+			{
+				return true;
+			}
+			return iter == other.iter;
+		}
+
+		// Проверка на неравенство
+		bool operator!=( const Iterator& other ) const 
+		{
+			return !operator==( other );
+		}
+
+		// Получение значения текущего узла
+		T operator*() const 
+		{
+			return *iter;
+		}
+
+		// Переход к следующему узлу
+		void operator++() 
+		{
+			iter++;
+		}
+
+	};
+
+private: 
+
+	std::map<cell_idx_type, T> m;
 
 
 public:
-	
-	using item = item_adapter<matrix<T, DEF_VAL, N>>;
-	using value_type = T;
 
-	// This gives us the "default" value to return for an empty bucket.
-	// We just use the default constructor on T, the template type
+	/**
+	 * This gives us the "default" value to return for an empty bucket.
+	 * We just use the default constructor on T, the template type
+	 */
 	const T&  default_value() const
 	{
 		BOOST_LOG_TRIVIAL(info) << __PRETTY_FUNCTION__;
@@ -92,6 +169,9 @@ public:
 		static value_type defaultval = value_type(DEF_VAL);
 		return defaultval;
 	}
+
+
+
 
 	matrix(){}
 
@@ -109,7 +189,7 @@ public:
 		return m.size();
 	}
 
-	const T& get(int idx) const
+	const T& get(const cell_idx_type &idx) const
 	{
 		BOOST_LOG_TRIVIAL(info) << __PRETTY_FUNCTION__;
 				
@@ -121,7 +201,7 @@ public:
 		return default_value();
 	}
 
-	void set(int idx, const T &val)
+	void set(const cell_idx_type &idx, const T &val)
 	{
 		BOOST_LOG_TRIVIAL(info) << __PRETTY_FUNCTION__;
 				
@@ -139,6 +219,21 @@ public:
 		}
 		return;
 	}
+
+	/// Получить итератор на начало списка
+	Iterator<T> begin() const
+	{
+		// Итератор пойдет от головного элемента...
+		return Iterator<T>( this, m.begin() );
+	}
+
+	/// Получить итератор на конец списка
+	Iterator<T> end() const
+	{
+		return Iterator<T>( this, m.end() );
+	}
+
+
 
 };
 
